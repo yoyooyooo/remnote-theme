20854e1e73b83efc22a3f22a76237f43f6b155a3
yoyo <jihu777@gmail.com>
2021-05-07 14:59:02 +0800

    fix css

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-	//   parser: "postcss-less",
-	plugins: [
-		require("cssnano")({
-			preset: "default",
-		}),
-		require("postcss-import")(),
-		require("postcss-nested"),
-		//   require("postcss-url")({ url: "copy", useHash: true })
-	],
+  //   parser: "postcss-less",
+  plugins: [
+    require("cssnano")({
+      preset: "default",
+    }),
+    require("postcss-import")(),
+    require("postcss-nested"),
+    //   require("postcss-url")({ url: "copy", useHash: true })
+  ],
 };
